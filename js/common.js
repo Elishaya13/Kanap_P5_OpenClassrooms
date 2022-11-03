a33a8ab106a3f6f4c282ed80b626b633ee22988d
Angie pons <a.lartillier@gmail.com>
2022-11-03 21:35:48 +0100

    changement de facon de récuperer le chemin de base

--- js/common.js
+++ js/common.js
@@ -1,3 +1,3 @@
 //On déclare les chemins qui seront utilisés
-export const baseFrontURL = window.location.origin + "/html"
+export const baseFrontURL = window.location.href.replace("/index.html", "")
 export const apiUrl = "http://localhost:3000/api/products/"
