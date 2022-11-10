76d0f64235787eb3887284def9160c66f34c1ca8
Angie pons <a.lartillier@gmail.com>
2022-11-10 12:06:06 +0100

    Ajout de la fonction modification de la quantite dans le panier

--- js/cart.js
+++ js/cart.js
@@ -1,82 +1,63 @@
 import { apiUrl } from "./common.js"
 
 
-
-
-// /**
-//  * 
-//  * @returns {Promise<JSON>} Promise object represents the Json of the API
-//  */
-// const getProducts = async () => {
-//     let r = await fetch(apiUrl)
-//     let json = await r.json()
-//     return json
-// }
-
-
-
 // Recupere les données sauvegardées dans le localStorage (string) et les retourne sous forme de Json/Objet
 const getCart = () => {
 
-    let cart = localStorage.getItem("cart")
-    //si le localStorage est vide , retourne un tableau vide
-    if (cart == null) {
-
-        return [];
-        // sinon retourne la valeur recuperée (string) et la retourne en JSON
-    } else {
-
-        return JSON.parse(cart)
-    }
-
-
+  let cart = localStorage.getItem("cart")
+  if (cart == null) {
+    return [];
+  } else {
+    return JSON.parse(cart)
+  }
 }
 
-const fetchProduct = async (productId) => {
-    const r = await fetch(apiUrl + productId)
-    let jsonProduct = await r.json()
-    return jsonProduct
-}
-const sortProducts = () => {
-    productsInCart.sort(function (a, b) {
-        if (a.id < b.id)
-            return -1
-        if (a.id > b.id)
-            return 1
-    })
-}
-
-
-//recupere le panier en localstorage puis parcours les données
 /**
- * @param {JSON} products // Json recuperer de getCart()
+ * Transforms the data into a string 
+ * And saves it in the localStorage
+ * @param {JSON} cart 
+ */
+const saveCart = (cart) => {
+
+  localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+// Intérroge l'API du produit et récupere le Json
+const fetchProduct = async (productId) => {
+
+  const r = await fetch(apiUrl + productId)
+  let jsonProduct = await r.json()
+  return jsonProduct
+}
+
+
+
+/**
+ * Fuction showDetailsCart
+ * @param {JSON} productsInCart 
+ * 
  */
 const showDetailsCart = async (productsInCart) => {
 
-    let displayHtmlProduct = ''
+  let displayHtmlProduct = ''
 
-    // Pour chaque produit intérroge les données de l'API puis créer un contenu html
-    for (let product of productsInCart) {
+  // Boucle sur chaque produit et intérroge les données de l'API
+  // Puis créer un contenu html
+  for (let product of productsInCart) {
+    await fetchProduct(product.id)
+      .then(fetchProductJson => {
+        displayHtmlProduct += displayAProduct(product, fetchProductJson)
+      })
+  }
 
-        await fetchProduct(product.id).then(fetchProductJson => {
-            displayHtmlProduct += displayAProduct(product, fetchProductJson)
+  // Insère le contenu HTML dans le parent
+  document
+    .getElementById('cart__items')
+    .insertAdjacentHTML("beforeend", displayHtmlProduct)
 
-        })
-
-    }
-    // Insère le contenu HTML dans le parent
-    document.getElementById('cart__items').insertAdjacentHTML("beforeend", displayHtmlProduct)
-
-    // document.querySelectorAll('.itemQuantity').forEach(item => {
-    //     item.addEventListener('change', (item) => updateCart(item))
-    // })
-
-
+  //Ecoute si un changement est fait sur le panier 
+  changeCart()
 }
-// const updateCart = (item) => {
-//     let currentElt = item.closest("article");
-//     console.log(currentElt.getAttribute('data-id'))
-// }
 
 /**
  * Use 
@@ -85,8 +66,8 @@ const showDetailsCart = async (productsInCart) => {
  * @returns {string}
  */
 const displayAProduct = (product, fetchProductJson) => {
-    let productElt =
-        `
+  let productElt =
+    `
              <article class="cart__item" data-id="${product.id}" data-color="${product.color}">
                 <div class="cart__item__img">
                   <img src="${fetchProductJson.imageUrl}" alt="${fetchProductJson.altTxt}">
@@ -109,11 +90,52 @@ const displayAProduct = (product, fetchProductJson) => {
                 </div>
               </article>
          `
-    return productElt
+  return productElt
 }
 
+//Trie les produits par Id
+const sortProducts = () => {
 
+  productsInCart.sort(function (a, b) {
+    if (a.id < b.id)
+      return -1
+    if (a.id > b.id)
+      return 1
+  })
+}
 
+/**
+ * Update cart with new values of product quantity
+ */
+const changeCart = () => {
+
+  // Selectionne tous les inputs de quantité
+  document.querySelectorAll('.itemQuantity').forEach(item => {
+
+    // Cible le parent et recupère l'information id et color dans ses attributs
+    let currentElt = item.closest("article");
+    let currentEltId = currentElt.getAttribute('data-id')
+    let currentEltColor = currentElt.getAttribute('data-color')
+
+    // Ajoute un evenement d'ecoute au changement de valeur de l'input
+    item.addEventListener('change', (e) => changeQuantity(e.target.value))
+
+    /**
+     * @param {number} targetValue
+     * Parcours l'actuel panier, modifie la quantité par la nouvelle valeur saisie
+     */
+    const changeQuantity = (targetValue) => {
+
+      let cart = getCart()
+
+      let foundProduct = cart.find(p => p.id == currentEltId && p.color == currentEltColor)
+      if (foundProduct != undefined) {
+        foundProduct.quantity = targetValue
+        saveCart(cart)
+      }
+    }
+  })
+}
 
 const productsInCart = getCart()
 showDetailsCart(productsInCart), sortProducts()
